c102cf3bc50d5d9e1d19af57bfaeb16a1438f57e
David Luna <davidrlunag@gmail.com>
2025-05-25 07:28:01 -0600

    Cambios a props

--- clase_01/src/components/Bienvenida/Bienvenida.jsx
+++ clase_01/src/components/Bienvenida/Bienvenida.jsx
@@ -1,10 +1,10 @@
-function App() {
+function App({titulo,descripcion,url}) {
   return (
     <div className="app-container">
-      <h1>¡Bienvenido al Banco de Costa Rica!</h1>
-      <p>Estamos construyendo tu experiencia bancaria del futuro.</p>
+      <h1>{titulo}</h1>
+      <p>{descripcion}</p>
       <img 
-        src="https://logos-world.net/wp-content/uploads/2021/03/Scotiabank-Logo.png" 
+        src={`"${url}"`}
         alt="Logo Scotia Bank" 
         width="200"
       />
@@ -12,10 +12,7 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
 {/*
-Crear un nuevo elemento div con una clase "promo-banner"
-Dentro agregar un título h2 con el texto "Promoción Especial"
-Añadir un párrafo con el texto "Obtén un 1% extra en intereses este mes"
-Incluir un botón que diga "Aplicar ahora"
+Crear un nuevo componente llamado Interno y se le pase el titulo y la descripcion"
 */}
--- clase_01/src/main.jsx
+++ clase_01/src/main.jsx
@@ -7,7 +7,7 @@ import ProductCardApp from './components/ProductCard/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BienvenidaApp></BienvenidaApp>
+    <BienvenidaApp titulo="¡Bienvenido a Scotia Bank" descripcion="Estamos construyendo tu experiencia bancaria del futuro." url="https://logos-world.net/wp-content/uploads/2021/03/Scotiabank-Logo.png"></BienvenidaApp>
     <ProductCardApp></ProductCardApp>
     <AccountApp></AccountApp>
   </StrictMode>,
